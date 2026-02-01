907f7c138d368ba9e6ce8b3e43d3d9ebfd19229d
DIAN F CARVALHO <202601103792@alunos.estacio.br>
2026-02-01 14:56:26 -0300

    Format output and input prompts for PIB and points

--- super_trunfo.c
+++ super_trunfo.c
@@ -28,8 +28,8 @@ void exibir_carta(Carta c) {
     printf("Nome da Cidade: %s \n", c.nome);
     printf("População: %d\n", c.populacao);
     printf("Área: %.2f \n", c.area);
-    printf("PIB: %.2f bilhões de reais", c.pib);
-    printf("Número de Pontos Turísticos: %d", c.pturistico); 	
+    printf("PIB: %.2f bilhões de reais \n", c.pib);
+    printf("Número de Pontos Turísticos: %d \n", c.pturistico); 	
     printf("Densidade: %.2f hab/km²\n", c.densidade);
     printf("---------------------------\n");
 }
@@ -44,11 +44,15 @@ int main() {
     printf("Nome Cidade 1: ");   	 scanf("%s", c1.nome);
     printf("População: ");       	 scanf("%d", &c1.populacao);
     printf("Área: ");            	 scanf("%f", &c1.area);
+    printf("PIB: ");            	 scanf("%f", &c1.pib);
+    printf("Pontos Turísticos: "); 	 scanf("%d", &c1.pturistico);
     
 
     printf("\nNome Cidade 2: "); 	 scanf("%s", c2.nome);
     printf("População: ");      	 scanf("%d", &c2.populacao);
     printf("Área: ");           	 scanf("%f", &c2.area);
+    printf("PIB: ");            	 scanf("%f", &c2.pib);
+    printf("Pontos Turísticos: "); 	 scanf("%d", &c2.pturistico);
 
     // PARTE LÓGICA
     c1 = calcular_dados(c1);
